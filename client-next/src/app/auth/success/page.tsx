d3e4eefb3c3d562ba6f6e1fe95c6fd41bba5e46b
Yared <alemayehuyared9@gmail.com>
2026-02-09 07:49:11 -0800

    Fix Next.js page export

---
+++ client-next/src/app/auth/success/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function SuccessPage() {
+  return (
+    <div>
+      <h1>Authentication Successful</h1>
+      <p>You can now close this page.</p>
+    </div>
+  );
+}
